2f31f7c09e24943d5352d31fb5ba39ff257f9fcf
KetanBasi <fatanaqilla90@gmail.com>
2021-02-11 01:50:28 +0700

    Small cleanup and rearrangement + ~5 lines improvement

--- Clean Start-Cut.py
+++ Clean Start-Cut.py
@@ -14,177 +14,7 @@ inTest = cfg.getboolean('Main', 'inTest')
 # Working location
 if inTest:
     StartDir = cfg.get('Main', 'StartTestDir')
-    Database = DBSample
-else:
-    StartDir = cfg.get('Main', 'StartMenuDir')
-
-DBStyle = cfg.getint('Main', 'DBStyle')
-DBFileName = cfg.get('Main', 'DBFile')
-# ! Untested
-# TODO: Add .csv & cusom style support
-if DBStyle == 0:
-    with open(DBFileName, 'r') as _DBFile:
-        DBFile = _DBFile
-
-threshold = cfg.getfloat('Main', 'Sensitivity')
-maxDepth = cfg.getint('Main', 'MaxDepth')
-
-conFil = [
-'help',
-'uninstall',
-'homepage',
-'edit',
-'config',
-'eula',
-'faq',
-'readme',
-'release note',
-'website',
-'documentation',
-'visit',
-'license',
-'manual',
-'docs'
-]
-
-# ANCHOR: Functions
-def getFilteredList(getFolderNotFile=True):
-    result = []
-    for item in filesOnCurrentDir:
-        try:
-            os.chdir(item)
-            if getFolderNotFile:
-                result.append(item)
-            os.chdir('..')
-        except NotADirectoryError:
-            if not getFolderNotFile:
-                result.append(item)
-    return result
-
-def moveShortCut(file, location, ext='.lnk', move_up=True):
-    try:
-        if file.endswith(ext) and move_up:
-            os.rename(f'{location}/{file}', file)
-        elif file.endswith(ext) and not move_up:
-            os.rename(file, f'{location}/{file}')
-        return True
-    except FileNotFoundError:
-        return False
-
-def moveAll(files, move_up=True):
-    # *
-    # *
-    if not move_up and os.getcwd().endswith('Start Menu'):
-        target = 'Programs'
-    elif move_up and '\\Start Menu\\Programs\\' in os.getcwd():
-        target = '..'
-    else: target = '.'
-
-    for item in files:
-        if not move_up:
-            moveShortCut(item, location=target, move_up=move_up)
-        else:
-            folders = getFilteredList()
-            
-
-def findMatch(match, options):
-    results = []
-    for item in options:
-        print(f'Matching \'{match}\' with \'{item}\'')
-        ratio = fuzz.ratio(match, item)
-        partialRatio = fuzz.partial_ratio(match, item)
-        tokenSortRatio = fuzz.token_sort_ratio(match, item)
-        tokenSetRatio = fuzz.token_set_ratio(match, item)
-        score1 = 0
-        for j in [ratio, partialRatio, tokenSortRatio, tokenSetRatio]:
-            if j >= threshold: score1 += 1
-        score2 = (ratio + tokenSortRatio) / 2
-        results.append( (item, score1, score2) )
-        print(f'Score 1: {score1}')
-        print(f'Score 2: {score2}')
-    score2 = [results[i][2] for i in range(len(results))]
-    highest = results[score2.index(max(score2))]
-    print(score2)
-    print(f'Highest: {highest[0]}\n\tScore:{highest[2]}')
-    return highest[0]
-
-def findTarget(match, options):
-    result = []
-    confidence = []
-    for i in range(len(options)):
-        _result = findMatch(match, options)
-        _confidence = 100
-        for e in conFil:
-            if e in _result.lower():
-                _confidence -= len(conFil) + 5
-        result.append(_result)
-        confidence.append(_confidence)
-        print(f'{_result}: {_confidence}')
-        if _confidence < 100:
-            if len(result) > 1 and confidence[-2] < confidence[-1]:
-                return result[-1]
-            elif len(result) > 1 and confidence[-2] > confidence[-1]:
-                return result[-2]
-            else:
-                confidence.append(_confidence)
-                options.remove(_result)
-                continue
-        else:
-            return result[-1]
-
-def getFileFolderList(dir='.'):
-    os.chdir(dir)
-    filesOnCurrentDir = os.listdir()
-    selectedFiles = [ (Database['Allowed'][program]['FolderName'], Database['Allowed'][program]['Target']) for program in Database['Allowed'] ]
-    filterFiles = Database['SysFile']['Files']
-    _Unallowed = [Database['SysFile']['Directories']] + [[Database['Unallowed']['NoGroup'][i] for i in Database['Unallowed']['NoGroup']]] + [Database['Unallowed']['Groups'][i] for i in Database['Unallowed']['Groups']]
-    Unallowed = list(set(itertools.chain.from_iterable(_Unallowed)))
-    try:
-        Unallowed.remove('')
-    except ValueError:
-        pass
-    filters = set(filterFiles + Unallowed + ['Programs'])
-    return set(filesOnCurrentDir) - filters
-
-
-# ANCHOR: Main
-# os.chdir('test_dir/Start Menu')
-os.chdir(StartDir)
-
-# * V1
-# filesOnCurrentDir = os.listdir()
-# selectedFiles = [ (Database['Allowed'][program]['FolderName'], Database['Allowed'][program]['Target']) for program in Database['Allowed'] ]
-# filterFiles = Database['SysFile']['Files']
-# _Unallowed = [Database['SysFile']['Directories']] + [[Database['Unallowed']['NoGroup'][i] for i in Database['Unallowed']['NoGroup']]] + [Database['Unallowed']['Groups'][i] for i in Database['Unallowed']['Groups']]
-# Unallowed = list(set(itertools.chain.from_iterable(_Unallowed)))
-# try: Unallowed.remove('')
-# except ValueError: pass
-# filters = set(filterFiles + Unallowed + ['Programs'])
-# filesOnCurrentDir = set(filesOnCurrentDir) - filters
-# * V2
-filesOnCurrentDir = getFileFolderList()
-
-# ? Move any shortcut that placed outside 'Programs' folder into it
-# * V1
-# for item in filesOnCurrentDir:
-#     if item.endswith('.lnk'):
-#         os.rename(item, f'Programs/{item}')
-# * V2
-moveAll(filesOnCurrentDir, move_up=False)
-
-os.chdir('Programs')
-
-filesOnCurrentDir = getFileFolderList()
-
-# TODO: Find out how to extract shortcut to "Start Menu/Programs/"
-# ? Use Allowed, Unallowed, SysFile, String Match, and Confidence rule
-
-# ANCHOR: =========
-
-if __name__ == '__main__':
-    # Something here
-    _ = None # ! Dummy line
-    DBSample = {
+    Database = {
 
         'Allowed': {
             # ? <Name>: {
@@ -349,5 +179,162 @@ if __name__ == '__main__':
                 'Windows Kits', 'Windows PowerShell', 'Windows System'
             ]
         }
-
     }
+else:
+    StartDir = cfg.get('Main', 'StartMenuDir')
+
+DBStyle = cfg.getint('Main', 'DBStyle')
+DBFileName = cfg.get('Main', 'DBFile')
+
+# !         Untested
+# TODO:     Add .csv & cusom style support
+if DBStyle == 0:
+    with open(DBFileName, 'r') as _DBFile:
+        DBFile = _DBFile
+
+threshold = cfg.getfloat('Main', 'Sensitivity')
+maxDepth = cfg.getint('Main', 'MaxDepth')
+
+filesOnCurrentDir = []
+
+conFil = [
+    'help',
+    'uninstall',
+    'homepage',
+    'edit',
+    'config',
+    'eula',
+    'faq',
+    'readme',
+    'release note',
+    'website',
+    'documentation',
+    'visit',
+    'license',
+    'manual',
+    'docs'
+]
+
+# ANCHOR: Functions
+def moveShortCut(file, location, ext='.lnk', move_up=True):
+    try:
+        if file.endswith(ext) and move_up:
+            os.rename(f'{location}/{file}', file)
+        elif file.endswith(ext) and not move_up:
+            os.rename(file, f'{location}/{file}')
+        return True
+    except FileNotFoundError:
+        return False
+
+def moveAll(files, move_up=True):
+    if not move_up and os.getcwd().endswith('Start Menu'):
+        target = 'Programs'
+    elif move_up and '\\Start Menu\\Programs\\' in os.getcwd():
+        target = '..'
+    else: target = '.'
+
+    for item in files:
+        if not move_up:
+            moveShortCut(item, location=target, move_up=move_up)
+        else:
+            # folders = getFilteredList()
+            try:
+                os.chdir(item)
+                itemInsideFolder = getFilteredList(getFolder=False)
+                targetItem = findTarget(item, itemInsideFolder)
+                os.chdir('..')
+                moveShortCut(targetItem, item)
+            except NotADirectoryError:
+                continue
+
+def findMatch(match, options):
+    results = []
+    for item in options:
+        print(f'Matching \'{match}\' with \'{item}\'')
+        ratio = fuzz.ratio(match, item)
+        partialRatio = fuzz.partial_ratio(match, item)
+        tokenSortRatio = fuzz.token_sort_ratio(match, item)
+        tokenSetRatio = fuzz.token_set_ratio(match, item)
+        score1 = 0
+        for j in [ratio, partialRatio, tokenSortRatio, tokenSetRatio]:
+            if j >= threshold: score1 += 1
+        score2 = (ratio + tokenSortRatio) / 2
+        results.append( (item, score1, score2) )
+        print(f'Score 1: {score1}')
+        print(f'Score 2: {score2}')
+    score2 = [results[i][2] for i in range(len(results))]
+    highest = results[score2.index(max(score2))]
+    print(score2)
+    print(f'Highest: {highest[0]}\n\tScore:{highest[2]}')
+    return highest[0]
+
+# ! [10/02/2020 0:24AM GMT+7]: Not sure how accurate is this
+def findTarget(match, options):
+    result = []
+    confidence = []
+    for i in range(len(options)):
+        _result = findMatch(match, options)
+        _confidence = 100
+        for e in conFil:
+            if e in _result.lower():
+                _confidence -= len(conFil) + 5
+        result.append(_result)
+        confidence.append(_confidence)
+        print(f'{_result}: {_confidence}')
+        if _confidence < 100:
+            if len(result) > 1 and confidence[-2] < confidence[-1]:
+                return result[-1]
+            elif len(result) > 1 and confidence[-2] > confidence[-1]:
+                return result[-2]
+            else:
+                confidence.append(_confidence)
+                options.remove(_result)
+                continue
+        else:
+            return result[-1]
+
+def getFileFolderList(dir='.'):
+    os.chdir(dir)
+    filesOnCurrentDir = os.listdir()
+    selectedFiles = [ (Database['Allowed'][program]['FolderName'], Database['Allowed'][program]['Target']) for program in Database['Allowed'] ]
+    filterFiles = Database['SysFile']['Files']
+    _Unallowed = [Database['SysFile']['Directories']] + [[Database['Unallowed']['NoGroup'][i] for i in Database['Unallowed']['NoGroup']]] + [Database['Unallowed']['Groups'][i] for i in Database['Unallowed']['Groups']]
+    Unallowed = list(set(itertools.chain.from_iterable(_Unallowed)))
+    try:
+        Unallowed.remove('')
+    except ValueError:
+        pass
+    filters = set(filterFiles + Unallowed + ['Programs'])
+    return set(filesOnCurrentDir) - filters
+
+def getFilteredList(getFolder=True):
+    global filesOnCurrentDir
+    result = []
+    for item in filesOnCurrentDir:
+        try:
+            os.chdir(item)
+            if getFolder:
+                result.append(item)
+            os.chdir('..')
+        except NotADirectoryError:
+            if not getFolder:
+                result.append(item)
+    return result
+
+def moveWork(target):
+    global filesOnCurrentDir
+    os.chdir(target)
+    filesOnCurrentDir = getFileFolderList()
+
+# ANCHOR: Main
+moveWork(StartDir)
+moveAll(filesOnCurrentDir, move_up=False)
+
+moveWork('Programs')
+filesOnCurrentDir = getFilteredList()
+moveAll(filesOnCurrentDir)
+
+# TODO: Find out how to extract shortcut to "Start Menu/Programs/"
+# ? Use Allowed, Unallowed, SysFile, String Match, and Confidence rule
+
+# ANCHOR: =========
